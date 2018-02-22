4c502ca1335315cba2e802975647890bc8570f00
Elizabeth <jukilove.c@gmail.com>
2018-02-21 20:17:11 -0800

    initall comment

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+#platform :ios, '9.0'
+
+target 'instagram_demo' do
+# Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+    use_frameworks!
+
+
+    # Pods for instagram_demo
+     pod 'Parse'
+
+    target 'instagram_demoTests' do
+        inherit! :search_paths
+        # Pods for testing
+     pod 'Parse'
+    end
+
+    target 'instagram_demoUITests' do
+        inherit! :search_paths
+        # Pods for testing
+     pod 'Parse'
+    end
+
+end
+
---
+++ instagram_demo/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  instagram_demo
+//
+//  Created by Elizabeth on 2/21/18.
+//  Copyright © 2018 Elizabeth. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onLogIn(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+            }
+        }
+    }
+    
+
+}
